2aa390913bca054df56be91787ce3dc7ecdcf5ce
dir-FY24murataryo <ryo.murata.g095@dirbato.co.jp>
2024-04-08 11:10:47 +0900

    チャプター4までコミット

--- OopBasic/src/company/Employee.java
+++ OopBasic/src/company/Employee.java
@@ -1,9 +1,10 @@
 package company;
 
-public class Employee implements Workable {
+// 抽象クラスに変更
+public abstract class Employee implements Workable {
     // フィールド
     protected final String name;
-    private final Department department;
+    protected final Department department; // protectedに変更
     private final String position;
     private final int employeeId;
 
@@ -24,12 +25,10 @@ public class Employee implements Workable {
     public void report() {
         report(1);
     }
-    
+
+    // 抽象メソッドに変更    
     // 会議に参加するメソッド
-    public void joinMeeting() {
-        department.meeting();
-        System.out.println("→上記の会議に参加します。部署：" + department.getName() + "名前：" + name);
-    }
+    public abstract void joinMeeting();
 
     // 働くメソッドを実装
     @Override
